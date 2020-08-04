8e69b1935945fd138c5950933a29a1b161767538
Y-Meta <y_meta@163.com>
2020-08-04 20:47:40 +0800

    'a'

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,9 +1,24 @@
 <template>
-  <el-carousel indicator-position="outside">
-    <el-carousel-item v-for="item in carouselList" :key="item.key">
-      <img :src="item.src">
-    </el-carousel-item>
-  </el-carousel>
+  <div class="main">
+    <el-carousel indicator-position="outside">
+      <el-carousel-item v-for="item in carouselList" :key="item.key">
+        <img :src="item.src" />
+      </el-carousel-item>
+    </el-carousel>
+    <div class="time-line">
+      <el-timeline>
+        <el-timeline-item
+          v-for="(activity, index) in activities"
+          :key="index"
+          :icon="activity.icon"
+          :type="activity.type"
+          :color="activity.color"
+          :size="activity.size"
+          :timestamp="activity.timestamp"
+        >{{activity.content}}</el-timeline-item>
+      </el-timeline>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -13,7 +28,25 @@
     carouselList: [
       {key: 'sadas', src: 'https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=1906432669,805384777&fm=26&gp=0.jpg', desc: '111'},
       {key: 'dksaj', src: 'https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=1636028025,2351994723&fm=26&gp=0.jpg', desc: '222'}
-    ]
+    ],
+     activities: [{
+          content: '支持使用图标',
+          timestamp: '2018-04-12 20:46',
+          size: 'large',
+          type: 'primary',
+          icon: 'el-icon-more'
+        }, {
+          content: '支持自定义颜色',
+          timestamp: '2018-04-03 20:46',
+          color: '#0bbd87'
+        }, {
+          content: '支持自定义尺寸',
+          timestamp: '2018-04-03 20:46',
+          size: 'large'
+        }, {
+          content: '默认样式的节点',
+          timestamp: '2018-04-03 20:46'
+        }]
     }
   }
   };
@@ -21,19 +54,26 @@
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-  .el-carousel__item h3 {
-    color: #475669;
-    font-size: 18px;
-    opacity: 0.75;
-    line-height: 300px;
-    margin: 0;
-  }
+body {
+  margin: 0;
+  padding: 0;
+}
+.time-line {
   
-  .el-carousel__item:nth-child(2n) {
-    background-color: #99a9bf;
-  }
-  
-  .el-carousel__item:nth-child(2n+1) {
-    background-color: #d3dce6;
-  }
+}
+.el-carousel__item h3 {
+  color: #475669;
+  font-size: 18px;
+  opacity: 0.75;
+  line-height: 300px;
+  margin: 0;
+}
+
+.el-carousel__item:nth-child(2n) {
+  background-color: #99a9bf;
+}
+
+.el-carousel__item:nth-child(2n + 1) {
+  background-color: #d3dce6;
+}
 </style>
