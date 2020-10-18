972840f68f476acaeab19341874b9671add1f13d
Y-meta <y_meta@163.com>
2020-10-18 16:22:48 +0800

    初始化

--- src/main.js
+++ src/main.js
@@ -1,21 +1,18 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import '../theme/index.css'
+import store from './store/'
 import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
-import echarts from 'echarts'
+import 'element-ui/lib/theme-default/index.css'
 
-Vue.prototype.$echarts = echarts
+Vue.config.productionTip = false;
 
-Vue.config.productionTip = false
-Vue.use(ElementUI)
+Vue.use(ElementUI);
 
-/* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  router,
-  render: h => h(App)
+	el: '#app',
+	router,
+	store,
+	template: '<App/>',
+	components: { App }
 })
